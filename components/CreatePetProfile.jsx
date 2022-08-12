63268c85e161e9577b1be5acdf67f479388ef2e4
Ladd1993 <aladd1993@gmail.com>
2022-08-11 19:31:45 -0500

    &apos;

--- components/CreatePetProfile.jsx
+++ components/CreatePetProfile.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { FaPaw } from 'react-icons/fa'
-import { AiOutlineClose } from 'react-icons/ai'
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import TextInputs from '../components/TextInputs'
@@ -26,18 +25,16 @@ const PetProfileForm = () => {
 
     return (
         <div className="mt-24 mb-20 py-10 px-5 flex-[5]">
-            <h1 className="text-h1  justify-center items-center flex">Pet <FaPaw className="ml-5" /> file</h1>
-            <form className="pt-10 max-w-2xl mx-auto grid md:grid-cols-2 gap-x-14 items-center gap-5 ">
+            <h1 className="text-h1 justify-center items-center flex">Pet <FaPaw className="ml-5" /> file</h1>
+            <form className="pt-10 max-w-2xl mx-auto grid md:grid-cols-2 gap-x-14 items-center gap-5">
 
-                {/* Medications, neutered / spayed (checkbox), delete profile button  */}
                 <fieldset className="flex flex-col gap-y-2 border-none max-w-[430px] w-full relative md:col-span-2 justify-self-center mb-14">
                     {upload && (
                         <ToastContainer />
                     )}
                     <label htmlFor="petPhoto" className="px-10 relative h-72 w-full rounded-lg bg-white border-dashed border-2 border-slate-600 flex items-center justify-center overflow-hidden cursor-pointer">
                         <p className="w-full text-center text-lg">
-                            Click to Upload
-                            Your Pet's Favorite Photo!
+                            Click to Upload Your Pet&apos;s Favorite Photo!
                         </p>
                     </label>
 
